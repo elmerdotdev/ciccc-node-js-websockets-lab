1cf298f8c200a35b578cf14a99d286a7aba713f1
Elmer Balbin <contact@elmer.dev>
2024-11-29 10:45:35 -0800

    updated chat model

--- backend/src/models/chat.model.ts
+++ backend/src/models/chat.model.ts
@@ -5,7 +5,6 @@ const ChatSchema = new mongoose.Schema(
     username: { type: String, required: true },
     message: { type: String, required: true },
     room: { type: String },
-    timestamp: { type: Date, default: Date.now },
   },
   { timestamps: true }
 );
